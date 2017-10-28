a90cb3306eaf08a6a08bac7c9fc0d49cb98664d1
Shubhnik Singh <shubhniksingh@gmail.com>
2017-10-28 20:04:01 +0530

    bug fixed in navReducer

--- src/Reducers/navigationReducer.js
+++ src/Reducers/navigationReducer.js
@@ -2,7 +2,7 @@ import AppNavigator from '../Navigation/navigationStack'
 const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('screen1'))
 console.log(`*****NavState${JSON.stringify(initialState)}`);
 const navigationReducer = (state = initialState, action) => {
-    const newState = AppNavigator.router.getStateForAction(action)
+    const newState = AppNavigator.router.getStateForAction(action, state)
     return newState || state;
 }
 
